d622cf1048d627cfab8a7b5c2add77d99e6e8ffd
Amit Sharma <amitsharma14393@gmail.com>
2018-01-21 15:12:55 +0530

    setting up the structure

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,19 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
 
 <script>
-const dummy = () => {
-  console.log('testing dummy commit!');
-}
-dummy();
-// for dummy commit.
+
 export default {
   name: 'app'
 }
 </script>
 
-<style>
+<style lang="scss">
+ @import './assets/scss/main.scss';
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div class="todo">
+      Want to test the font style
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
